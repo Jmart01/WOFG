f79406b48f46b06fdceb18dc0709416e0631a8d4
Jmart01 <jonricmart01@gmail.com>
2022-11-27 23:01:42 -0600

    replaced default mode with BP_ShintoGameModeBase, built project from here

--- Source/Shinto/ShintoGameModeBase.cpp
+++ Source/Shinto/ShintoGameModeBase.cpp
@@ -5,14 +5,5 @@
 
 AShintoGameModeBase::AShintoGameModeBase()
 {
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/SHINTO_Content/Player/M_CharacterBase"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
-	else
-	{
-
-	}
+	
 }
